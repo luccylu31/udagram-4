8336dc694d79f3ab656f7f4d76dbdf7d63522597
luccylu31 <luccylu31@gmail.com>
2024-07-08 00:26:42 +0700

    Update CircleCI config and deployment scripts

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-api-env.eba-9wd2s3ds.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
