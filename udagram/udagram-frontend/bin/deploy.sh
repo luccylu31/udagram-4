9d20b3b09d264e12f2c7cdf217657f8c459767c1
luccylu31 <luccylu31@gmail.com>
2024-07-08 19:21:48 +0700

    msg

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive ./www/ s3://user3354372-udagram/ --profile default
+aws s3 cp --recursive ./www/ s3://user3354372-udagram/ 
 aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://user3354372-udagram/
