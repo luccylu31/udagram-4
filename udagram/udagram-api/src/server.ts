117fcde67c546187ad8a1d5fd1a154754ada35b8
luccylu31 <luccylu31@gmail.com>
2024-07-09 19:44:14 +0700

    ms

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -2,15 +2,13 @@ import * as dotenv from "dotenv";
 import cors from 'cors';
 import express from "express";
 import { sequelize } from "./sequelize";
-
 import { IndexRouter } from "./controllers/v0/index.router";
-
 import bodyParser from "body-parser";
 import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
 (async () => {
   dotenv.config();
-  
+
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
@@ -25,24 +23,19 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS. 
   app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
+    origin: '*', 
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Origin', 'X-Requested-With', 'Content-Type', 'Accept',
       'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
+      'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'
     ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
+    preflightContinue: false,
   }));
 
   app.use("/api/v0/", IndexRouter);
@@ -55,7 +48,6 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   // Start the Server
   app.listen(port, () => {
     console.log(`Backend server is listening on port ${port}....`);
-    console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
 })();
